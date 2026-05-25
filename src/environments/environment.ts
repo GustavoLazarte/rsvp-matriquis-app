3dc598e356b0d9d9cbfdd8a8b3da25893619cacb
gustavo <gustucapo@gmail.com>
2026-05-24 22:47:52 -0400

    fix env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  supabaseUrl: 'https://kcipfxnirobwifclvcnp.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtjaXBmeG5pcm9id2lmY2x2Y25wIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk1OTU1ODksImV4cCI6MjA5NTE3MTU4OX0.oXGvWFsaGtPg_ZCGSPDbS6w5BYOXsjNQD_JD5eqCqlc',
+  supabaseUrl: '',
+  supabaseKey: '',
 };
